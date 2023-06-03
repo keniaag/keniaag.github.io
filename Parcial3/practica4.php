9c3f7a95d302ef89c2d43eca01073aedab2b61e2
keniaag <124410749+keniaag@users.noreply.github.com>
2023-06-02 18:06:13 -0700

    02/06/2023 Guerrero Parra Kenia Patricia

--- Parcial3/practica4.php
+++ Parcial3/practica4.php
@@ -3,6 +3,9 @@
 $nombre = $_POST["nombre"];
 $calificacion = 0;
 
+echo "<h2>R E S U L T A D O ! ! !</h2>";
+echo "<h3>Alumno: " .$nombre. "</h3><hr>";
+
 $pregunta1 = $_POST["pregunta1"];
 $pregunta2 = $_POST["pregunta2"];
 $pregunta3 = $_POST["pregunta3"];
@@ -14,75 +17,96 @@ $pregunta8 = $_POST["pregunta8"];
 $pregunta9 = $_POST["pregunta9"];
 $pregunta10 = $_POST["pregunta10"];
 
+
 if($pregunta1=="b"){
             $calificacion++;
-            echo "<img src='!!' width='3%'><br><br><br>";
+            echo "Pregunta 1: ";
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 1: ";
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta2=="a"){
             $calificacion++; 
-            echo "<img src='correcta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 2: ";
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";       
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 2: ";
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta3=="b"){
-            $calificacion++;   
-            echo "<img src='correcta.png' width='3%'><br><br><br>";     
+            $calificacion++;  
+            echo "Pregunta 3: "; 
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";     
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 3: ";
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta4=="a"){
-            $calificacion++;        
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            $calificacion++;  
+            echo "Pregunta 4: ";      
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 4: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta5=="c"){
             $calificacion++;
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            echo "Pregunta 5: ";  
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 5: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta6=="b"){
             $calificacion++;
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            echo "Pregunta 6: ";  
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 6: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta7=="a"){
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            echo "Pregunta 7: ";  
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
             $calificacion++;
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 7: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta8=="c"){
             $calificacion++;
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            echo "Pregunta 8: ";  
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 8: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta9=="a"){
             $calificacion++;
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            echo "Pregunta 9: ";  
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 9: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($pregunta10=="a"){
             $calificacion++;
-            echo "<img src='correcta.png' width='3%'><br><br><br>";
+            echo "Pregunta 10: ";  
+            echo "<img src='correcto.png' width='3%'><br><br><hr>";
         }else{
-            echo "<img src='incorrecta.png' width='3%'><br><br><br>";       
+            echo "Pregunta 10: ";  
+            echo "<img src='incorrecto.png' width='3%'><br><br><hr>";       
         }
         if($calificacion>5 && $calificacion<=8){
             echo "Felicidades!!! Pasaste con " .$calificacion;
-            echo "<img src='' width='3%'>";
+            echo "<img src='bien.png' width='3%'>";
 
         }elseif($calificacion<5){
             echo "No eres fan. Sacaste " . $calificacion;
-            echo "<img src='' width='3%'><br><br>";
+            echo "<img src='triste.png' width='3%'><br><br>";
         }elseif($calificacion>8){
             echo "Eres un fan de verdad!!! Terminaste con calificacion de ". $calificacion;
-            echo "<img src='' width='3%'><br><br>";
+            echo "<img src='bien.png' width='3%'><br><br>";
         }
     ?>
