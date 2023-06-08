c52726acc024679650146084f479add484e3c36c
keniaag <124410749+keniaag@users.noreply.github.com>
2023-06-07 18:29:23 -0700

    Guerrero Parra Kenia Patricia 07/06/23

--- Parcial3/practica5.php
+++ Parcial3/practica5.php
@@ -1,7 +1,6 @@
 <?php
+
     $cuadros=$_POST["cuadro"];
-    
-    //Validar que solo lleve "X" y "O"
     
     for($i=0; $i<count($cuadros); $i++){
         if($cuadros[$i]!="X" && $cuadros[$i]!="O" && $cuadros[$i]!="" ){
@@ -9,72 +8,111 @@
             return;
         }
     }
-    //Checar las horizontales de arriba
     if($cuadros[0]=="X" && $cuadros[1]=="X" && $cuadros[2]=="X"){
-        echo "<h1>Gana X"."<p style='color:blue';>[x][x][x]</p>[_][_][_]<br>[_][_][_]";
+        echo "<h1>Ganador [X]</h1>";
+        echo "[x][x][x]<br>";
+        echo "[_][_][_]<br>";
+        echo "[_][_][_]";
         return;
     }elseif($cuadros[0]=="O" && $cuadros[1]=="O" && $cuadros[2]=="O"){
-        echo "<h1>Gana O"."<p style='color:blue';>[x][x][x]</p>[_][_][_]<br>[_][_][_]";
+        echo "<h1>Ganador [O]</h1>";
+        echo "[o][o][o]<br>";
+        echo "[_][_][_]<br>";
+        echo "[_][_][_]";
         return;
     }
-    //Checar horizontal de en medio
     if($cuadros[4]=="X" && $cuadros[5]=="X" && $cuadros[3]=="X"){
-        echo "<h1>Gana X"."[_][_][_]<p style='color:blue';>[x][x][x]</p><br>[_][_][_]";;
+        echo "<h1>Ganador [X]</h1>";
+        echo "[_][_][_]<br>";
+        echo "[x][x][x]<br>";
+        echo "[_][_][_]";
         return;
     }elseif($cuadros[3]=="O" && $cuadros[4]=="O" && $cuadros[5]=="O"){
-        echo "<h1>Gana X"."[_][_][_]<p style='color:blue';>[x][x][x]</p><br>[_][_][_]";;
+        echo "<h1>Ganador [O]</h1>";
+        echo "[_][_][_]<br>";
+        echo "[o][o][o]<br>";
+        echo "[_][_][_]";
         return;
     }
-    //Checar las horizontales de abajo
-
     if($cuadros[6]=="X" && $cuadros[7]=="X" && $cuadros[8]=="X"){
-        echo "<h1>Gana X"."[_][_][_]<br>[_][_][_]<br><p style='color:blue';>[x][x][x]</p>";;
+        echo "<h1>Ganador [X]</h1>";
+        echo "[_][_][_]<br>";
+        echo "[_][_][_]<br>";
+        echo "[x][x][x]";
         return;
     }elseif($cuadros[6]=="O" && $cuadros[7]=="O" && $cuadros[8]=="O"){
-        echo "<h1>Gana O"."[_][_][_]<br>[_][_][_]<br><p style='color:blue';>[x][x][x]</p>";;
+        echo "<h1>Ganador [O]</h1>";
+        echo "[_][_][_]<br>";
+        echo "[_][_][_]<br>";
+        echo "[o][o][o]";
         return;
     }
 
-    
-    //Checar vertical izquierdo
     if($cuadros[0]=="X" && $cuadros[3]=="X" && $cuadros[6]=="X"){
-        echo "<h1>Gana X";
+        echo "<h1>Ganador [X]</h1>";
+        echo "[x][_][_]<br>";
+        echo "[x][_][_]<br>";
+        echo "[x][_][_]";
         return;
     }elseif($cuadros[0]=="O" && $cuadros[3]=="O" && $cuadros[6]=="O"){
-        echo "<h1>Gana O";
+        echo "<h1>Ganador [O]</h1>";
+        echo "[o][_][_]<br>";
+        echo "[o][_][_]<br>";
+        echo "[o][_][_]";
         return;
     }
-    //Checar vertical de en medio
     if($cuadros[1]=="X" && $cuadros[4]=="X" && $cuadros[7]=="X"){
-        echo "<h1>Gana X";
+        echo "<h1>Ganador [X]</h1>";
+        echo "[_][x][_]<br>";
+        echo "[_][x][_]<br>";
+        echo "[_][x][_]";
         return;
     }elseif($cuadros[1]=="O" && $cuadros[4]=="O" && $cuadros[7]=="O"){
-        echo "<h1>Gana O";
+        echo "<h1>Ganador [O]</h1>";
+        echo "[_][o][_]<br>";
+        echo "[_][o][_]<br>";
+        echo "[_][o][_]";
         return;
     }
-    //Checar vertical derecho
     if($cuadros[2]=="X" && $cuadros[5]=="X" && $cuadros[8]=="X"){
-        echo "<h1>Gana X";
+        echo "<h1>Ganador [X]</h1>";
+        echo "[_][_][x]<br>";
+        echo "[_][_][x]<br>";
+        echo "[_][_][x]";
         return;
     }elseif($cuadros[2]=="O" && $cuadros[5]=="O" && $cuadros[8]=="O"){
-        echo "<h1>Gana O";
+        echo "<h1>Ganador [O]</h1>";
+        echo "[_][_][o]<br>";
+        echo "[_][_][o]<br>";
+        echo "[_][_][o]";
         return;
     }
 
-    //Checar diagonales
     if($cuadros[0]=="X" && $cuadros[4]=="X" && $cuadros[8]=="X"){
-        echo "<h1>Gana X";
+        echo "<h1>Ganador [X]</h1>";
+        echo "[x][_][_]<br>";
+        echo "[_][x][_]<br>";
+        echo "[_][_][x]";
         return;
     }elseif($cuadros[0]=="O" && $cuadros[4]=="O" && $cuadros[8]=="O"){
-        echo "<h1>Gana O";
+        echo "<h1>Ganador [O]</h1>";
+        echo "[o][_][_]<br>";
+        echo "[_][o][_]<br>";
+        echo "[_][_][o]";
         return;
     }
 
     if($cuadros[2]=="X" && $cuadros[4]=="X" && $cuadros[6]=="X"){
-        echo "<h1>Gana X";
+        echo "<h1>Ganador [X]</h1>";
+        echo "[_][_][x]<br>";
+        echo "[_][x][_]<br>";
+        echo "[x][_][_]";
         return;
     }elseif($cuadros[2]=="O" && $cuadros[4]=="O" && $cuadros[6]=="O"){
-        echo "<h1>Gana O";
+        echo "<h1>Ganador [O]</h1>";
+        echo "[_][_][o]<br>";
+        echo "[_][o][_]<br>";
+        echo "[o][_][_]";
         return;
     }
 
