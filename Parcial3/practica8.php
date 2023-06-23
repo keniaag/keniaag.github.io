7dd6944e2247913ae445b74db005fd873646e454
keniaag <124410749+keniaag@users.noreply.github.com>
2023-06-22 18:46:32 -0700

    practica 8

--- Parcial3/practica8.php
+++ Parcial3/practica8.php
@@ -6,11 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica 8 - Juego de Lotería</title>
     <link rel="stylesheet" href="bootstrap.css">
+    <style>
+        .ficha{
+            background-image: url('ficha loteria.png');
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+        .color{
+            opacity: 0.3;
+        }
+    </style>
 </head>
 <body>
+
     <div class="container">
         <h1>Juego de la Lotería</h1><hr>
         <form action="practica8.php" method="POST">
+            <?php
+            if(isset($_POST["carta1"])){
+                $carta1 = $_POST["carta1"];
+                $carta2 = $_POST["carta2"];
+                //Aqui empieza la logica del juego
+                $carta = rand(1,54);
+                echo "<h2>Carta dada <img src='loteria/".$carta.".jpg' class='img-fluid' width='100px'></h2>";
+            }else{
+                $carta1 = [];
+                $total = 0;
+                while($total < 16){
+                    $numero = rand(1,54);
+                    if(array_search($numero, $carta1) === false){
+                        $carta1[$total] = $numero;
+                        $total ++;
+                    }
+                }
+
+                $carta2 = [];
+                $total = 0;
+                while($total < 16){
+                    $numero = rand(1,54);
+                    if(array_search($numero, $carta2) === false){
+                        $carta2[$total] = $numero;
+                        $total ++;
+                    }
+                }
+
+            }
+              
+            ?>
             <input type="submit" value="Dar carta" name="carta" class="btn btn-primary"><hr>
             <div class="row">
                 <div class="col-6">
@@ -20,6 +63,7 @@
                         for($i=0; $i<16; $i++){
                             $numero = rand(1,54);
                             echo "<div class='col-3 p-0'><img src='loteria/".$numero.".jpg' class='img-thumbnail'></div>";
+                            echo "<input type='hidden' name='carta1[]' value='". $carta1[$i] ."'>";
                         }
                     ?>
                     </div>
@@ -32,6 +76,7 @@
                         for($i=0; $i<16; $i++){
                             $numero = rand(1,54);
                             echo "<div class='col-3 p-0'><img src='loteria/".$numero.".jpg' class='img-thumbnail'></div>";
+                            echo "<input type='hidden' name='carta2[]' value='". $carta2[$i] ."'>";
                         }
                     ?>
                     </div>
